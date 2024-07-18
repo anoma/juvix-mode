e7b409a844a7d94e064f6e68f25d5a19b2d2501c
Jan Mas Rovira <janmasrovira@gmail.com>
2024-07-18 23:30:15 +0200

    use --ide-end-error-char

--- flycheck-juvix.el
+++ flycheck-juvix.el
@@ -7,21 +7,28 @@
   :group 'flycheck
   :link '(url-link :tag "Github" "https://github.com/anoma/juvix"))
 
+(defvar juvix-end-error-char "ת"
+  "An auxiliary character that ends Juvix error messages.")
+
+(defvar rx-message
+    `(message (one-or-more (eval `(not ,juvix-end-error-char))))
+    )
+
 (flycheck-define-checker juvix
   "A Juvix syntax checker."
   :command ("juvix"
             (eval juvix-global-flags)
-            "--only-errors" "--no-colors" "--stdin"
+            "--ide-end-error-char" (eval juvix-end-error-char) "--no-colors" "--stdin"
             (option-flag "--no-stdlib" juvix-disable-embedded-stdlib)
             (option "--stdlib-path" juvix-stdlib-path)
             "typecheck"
             source-original)
   :standard-input t
   :error-patterns
-  ((error line-start (file-name) ":" line ":" column ": error:" (message (one-or-more (not "ת"))))
-   (error line-start (file-name) ":" line ":" column "-" end-column ": error:" (message (one-or-more (not "ת"))))
-   (error line-start (file-name) ":" line "-" end-line ":" column ": error:" (message (one-or-more (not "ת"))))
-   (error line-start (file-name) ":" line "-" end-line ":" column "-" end-column ": error:" (message (one-or-more (not "ת")))))
+  ((error line-start (file-name) ":" line ":" column ": error:" (eval rx-message))
+   (error line-start (file-name) ":" line ":" column "-" end-column ": error:" (eval rx-message))
+   (error line-start (file-name) ":" line "-" end-line ":" column ": error:" (eval rx-message))
+   (error line-start (file-name) ":" line "-" end-line ":" column "-" end-column ": error:" (eval rx-message)))
   :modes juvix-mode)
 
 (add-to-list 'flycheck-checkers 'juvix)
