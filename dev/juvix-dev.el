4548f7f357c0c26a560066842a7e7f947c151f10
Jan Mas Rovira <janmasrovira@gmail.com>
2024-11-12 15:32:11 +0100

    refactor dev

---
+++ dev/juvix-dev.el
@@ -0,0 +1,11 @@
+;;; juvix-dev.el --- Major modes for working with Juvix's internal languages  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; See https://github.com/anoma/juvix-mode
+
+;;; Code:
+(push (expand-file-name "nockma" (file-name-directory (or load-file-name buffer-file-name))) load-path)
+(require 'nockma-mode)
+
+(provide 'juvix-dev)
+;;; juvix-dev.el ends here
